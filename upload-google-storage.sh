532adba7379f13cd44d430cc2f881491109afce7
Vipul Swarup <vipul.swarup@eisenvault.com>
2015-09-15 21:56:29 +0530

    Added logging

--- upload-google-storage.sh
+++ upload-google-storage.sh
@@ -1,6 +1,6 @@
 #copy file name from /opt/backup/bakfile.txt
 value=$(</opt/backup/bakfile.txt)
-echo "Bak file name is $value \n"
+echo "Bak file name is $value"
 echo "Uploading $value"
 #Copy backup to google cloud bucket
-gsutil -o GSUtil:parallel_composite_upload_threshold=150M cp $value gs://argali_vtiger_backup/
+/usr/local/bin/gsutil -o GSUtil:parallel_composite_upload_threshold=150M cp $value gs://argali_vtiger_backup/ > /opt/backup/upload.log 2>&1
