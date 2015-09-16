388def9c12d4a099fa7a820069e07f0d78cf700b
Vipul Swarup <vipul.swarup@eisenvault.com>
2015-09-16 12:29:10 +0530

    Backing up vtiger application as well

--- backup_vtiger.sh
+++ backup_vtiger.sh
@@ -1,16 +1,31 @@
 #script to dump vtiger mysql database
 timestamp="$(date +"%Y-%m-%d_%H-%M-%S")"
 bak_folder=/opt/backup
-bak_file="$bak_folder/vtiger_backup_$timestamp.sql"
+bak_folder_2="$bak_folder/vtiger_backup_$timestamp"
+
+bak_file=vtiger_bak_$timestamp
 
 mkdir $bak_folder
+mkdir $bak_folder_2
 
-mysqldump -u root vtiger_db > "$bak_file"
+
+mysqldump -u root vtiger_db > "$bak_folder_2/sql_dump.sql"
+
+
+#copy the vtiger install folder as well
+tar -cf "$bak_folder_2/vtigercrm.tar" /var/www/vtigercrm/
 
 #check for pigz - multi threaded zip utility
 apt-get install pigz
+
+#tar the bak folder
+tar -cf $bak_folder/$bak_file.tar $bak_folder_2
+
 #zip the backup
-pigz --best $bak_file
+pigz --best $bak_folder/$bak_file.tar
+
+#delete temp bak folder
+rm -rf $bak_folder_2
 
 #delete previous backup
 #copy file name from /opt/backup/bakfile.txt
@@ -19,4 +34,4 @@ echo "Deleting old backup file: $old_bak_file_name"
 rm "$old_bak_file_name"
 
 #write name of created file in a txt file for uploader script to pick up
-echo "$bak_file.gz" > /opt/backup/bakfile.txt
+echo "$bak_folder/$bak_file.tar.gz" > /opt/backup/bakfile.txt
